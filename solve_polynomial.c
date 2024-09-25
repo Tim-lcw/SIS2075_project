48125876b6453aa7f3229a2431a75aec54e06318
Timothy Lew <lew01.timothy@gmail.com>
2024-09-25 23:03:14 +0400

    first function Homer's method eval z

--- solve_polynomial.c
+++ solve_polynomial.c
@@ -2,3 +2,13 @@
 #include <complex.h> // Used to implement complex numbers
 #include <math.h> // Used to implement mathematical functions
 #include <stdbool.h>  // For boolean values
+
+
+//Function to evaluate polynomial at complex z using Horner's Method
+complex double eval_polynomial(complex double coeffs[], int deg, complex double z) {
+    complex double sum = coeffs[0];
+    for (int i = 1; i <= deg; i++) {
+        sum = sum * z + coeffs[i];
+    }
+    return sum;
+}
