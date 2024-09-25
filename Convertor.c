fd4001beff7ff47d2f334098fe87fefee73ca6f9
Timothy Lew <lew01.timothy@gmail.com>
2024-09-26 01:46:27 +0400

    addition of while loop and exit to main of convertor

--- Convertor.c
+++ Convertor.c
@@ -44,6 +44,7 @@ long long convertBinaryToDecimal(long long n){
 
 int main(){
     int choice;
+     while (1){
     printf("Select an operation to perform:\n");
     printf("1. Convert Hexadecimal to Denary\n");
     printf("2. Convert Hexadecimal to Binary\n");
@@ -51,9 +52,11 @@ int main(){
     printf("4. Convert Decimal to Binary\n");
     printf("5. Convert Binary to Hexadecimal\n");
     printf("6. Convert Binary to Decimal\n");
-    printf("Insert choice [1-6]: ");
+    printf("7. Exit\n");
+    printf("Insert choice [1-7]: ");
     scanf("%d", &choice);
-
+   
+    
     switch (choice) {
         case 1: { // Hexadecimal to Denary
             int n;
@@ -103,11 +106,16 @@ int main(){
             long long n;
             printf("Enter a binary number: ");
             scanf("%lld", &n);
-            printf("%lld in binary = %lld in decimal\n", n, convertBinaryToDecimal(n));
+            printf("%lld in binary is %lld in decimal\n", n, convertBinaryToDecimal(n));
             break;
         }
+        case 7: { 
+                printf("Exiting...\n");
+                return 0;
         default:
             printf("Invalid option\n");
     }
-    return 0;
+    }
+    
+}
 }
