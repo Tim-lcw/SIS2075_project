092f7f34a8e393b947140ea7be3b51eff21209ba
Timothy Lew <lew01.timothy@gmail.com>
2024-09-26 01:41:09 +0400

    main for convertor

--- Convertor.c
+++ Convertor.c
@@ -41,3 +41,73 @@ long long convertBinaryToDecimal(long long n){
     }
     return dec;
 }
+
+int main(){
+    int choice;
+    printf("Select an operation to perform:\n");
+    printf("1. Convert Hexadecimal to Denary\n");
+    printf("2. Convert Hexadecimal to Binary\n");
+    printf("3. Convert Decimal to Hexadecimal\n");
+    printf("4. Convert Decimal to Binary\n");
+    printf("5. Convert Binary to Hexadecimal\n");
+    printf("6. Convert Binary to Decimal\n");
+    printf("Insert choice [1-6]: ");
+    scanf("%d", &choice);
+
+    switch (choice) {
+        case 1: { // Hexadecimal to Denary
+            int n;
+            printf("Insert Hexadecimal value: ");
+            scanf("%x", &n);
+            printf("Denary number is: %d\n", n);
+            break;
+        }
+        case 2: { // Hexadecimal to Binary
+            int num;
+            printf("Insert Hexadecimal value: ");
+            scanf("%x", &num);
+            printf("Binary number is: ");
+            decToBi(num);
+            break;
+        }
+        case 3: { // Decimal to Hexadecimal
+            int num;
+            printf("Insert Decimal number: ");
+            scanf("%d", &num);
+            printf("Hexadecimal number is: %X\n", num);
+            break;
+        }
+        case 4: { // Decimal to Binary
+            int num;
+            printf("Insert Decimal number: ");
+            scanf("%d", &num);
+            printf("Binary number is: ");
+            decToBinary(num);
+            printf("\n");
+            break;
+        }
+        case 5: { // Binary to Hexadecimal
+            long int biNum, hexNum = 0, i = 1, remainder;
+            printf("Insert Binary number: ");
+            scanf("%ld", &biNum);
+            while (biNum != 0) {
+                remainder = biNum % 10;
+                hexNum = hexNum + remainder * i;
+                i = i * 2;
+                biNum = biNum / 10;
+            }
+            printf("Equivalent hexadecimal value: %lX\n", hexNum);
+            break;
+        }
+        case 6: { // Binary to Decimal
+            long long n;
+            printf("Enter a binary number: ");
+            scanf("%lld", &n);
+            printf("%lld in binary = %lld in decimal\n", n, convertBinaryToDecimal(n));
+            break;
+        }
+        default:
+            printf("Invalid option\n");
+    }
+    return 0;
+}
