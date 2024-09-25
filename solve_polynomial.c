27c0eb753035e1d41012827f89f48abff2d1683a
Timothy Lew <lew01.timothy@gmail.com>
2024-09-25 23:00:51 +0400

    Library used

---
+++ solve_polynomial.c
@@ -0,0 +1,4 @@
+#include <stdio.h>   // Standard input output library
+#include <complex.h> // Used to implement complex numbers
+#include <math.h> // Used to implement mathematical functions
+#include <stdbool.h>  // For boolean values
