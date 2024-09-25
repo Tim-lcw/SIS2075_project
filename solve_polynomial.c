7497a1923da8472a0fec59e5ffe63b8e936082e2
Timothy Lew <lew01.timothy@gmail.com>
2024-09-25 23:45:41 +0400

    Main for polynomial solver

--- solve_polynomial.c
+++ solve_polynomial.c
@@ -73,3 +73,25 @@ void jenkins_traub(complex double coeffs[], int deg, complex double roots[]){
         }
     }
 }
+
+int main() {
+int deg;
+printf("Enter the degree of the polynomial: "); //Prompt user to enter degree polynomial
+    scanf("%d", &deg);
+    complex double coeffs[deg + 1];
+    complex double roots[deg];
+    printf("Enter real coefficients of the polynomial:\n"); //Prompt user to enter real values
+    for (int i = 0; i <= deg; i++) {
+        double real;
+        printf("Coefficient of x^%d: ", deg - i);
+        scanf("%lf", &real);
+        coeffs[i] = real + 0 * I; 
+    }
+jenkins_traub(coeffs, deg, roots);
+printf("The roots of the polynomial are:\n");
+    for (int i = 0; i < deg; i++) {
+        printf("Root %d: %.5f + %.5fi\n", i + 1, creal(roots[i]), cimag(roots[i]));
+    }
+
+return 0;
+}
